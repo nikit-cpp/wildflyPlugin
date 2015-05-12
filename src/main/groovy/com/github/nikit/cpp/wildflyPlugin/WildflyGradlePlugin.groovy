4fefd24d2691b2a216823ee7e8353f217130809d
nik <nikit.cpp@yandex.ru>
2015-05-13 00:28:19 +0300

    fix GFileUtils

--- src/main/groovy/com/github/nikit/cpp/wildflyPlugin/WildflyGradlePlugin.groovy
+++ src/main/groovy/com/github/nikit/cpp/wildflyPlugin/WildflyGradlePlugin.groovy
@@ -4,6 +4,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.ResolvedDependency
+import org.gradle.util.GFileUtils
 
 /**
  * Created by nik on 12.05.15.
